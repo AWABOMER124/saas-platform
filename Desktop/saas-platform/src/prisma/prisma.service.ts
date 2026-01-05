359090ebcd3b65d164a3b834538e2255723a9c44
Awab Omer <119068858+AWABOMER124@users.noreply.github.com>
2026-01-05 16:06:12 +0300

    Update prisma.service.ts

--- Desktop/saas-platform/src/prisma/prisma.service.ts
+++ Desktop/saas-platform/src/prisma/prisma.service.ts
@@ -1,17 +1,13 @@
-import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
-
 @Injectable()
-export class PrismaService
-  extends PrismaClient
-  implements OnModuleInit, OnModuleDestroy
-{
-  async onModuleInit(): Promise<void> {
+export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
+  async onModuleInit() {
     await this.$connect();
   }
 
-  async onModuleDestroy(): Promise<void> {
+  async onModuleDestroy() {
     await this.$disconnect();
   }
 }
